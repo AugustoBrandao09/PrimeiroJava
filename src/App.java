c48469c499dd376ab55d0d0d4d83b17f486dbb75
AugustoBrandao09 <augustobrandao.engcomp@gmail.com>
2023-08-26 19:33:39 -0300

    oi

--- src/App.java
+++ src/App.java
@@ -14,14 +14,14 @@ public class App {
         int vetor[] = {4,38,5,9,10,15};
         System.out.println(Arrays.toString(is.executar(vetor)));  */
 
-        /* //SelectSort
+        //SelectSort
         SelectSort ss = new SelectSort();
         int vetor[] = {9,7,57,4,21,54};
-        System.out.println(Arrays.toString(ss.executar(vetor))); */        
+        System.out.println(Arrays.toString(ss.executar(vetor)));        
         
-        /* //Busca Binária
+        //Busca Binária
         Buscabinaria bb = new Buscabinaria();
-        System.out.println(bb.executar(vetor, 54, 0, 6)); */   
+        System.out.println(bb.executar(vetor, 54, 0, 5));   
 
         
         /* String a = JOptionPane.showInputDialog("oi"); 
